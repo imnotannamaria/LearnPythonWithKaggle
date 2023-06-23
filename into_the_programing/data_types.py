9ed209f4e207aaeb2b63356c7e962bb672bf29ba
Anna Maria <aannamariabr@outlook.com>
2023-06-23 13:11:09 -0300

    Into The Programming - Data types

---
+++ into_the_programing/data_types.py
@@ -0,0 +1,7 @@
+def cost_of_project(engraving, solid_gold):
+    cost = solid_gold * (100 + 10 * len(engraving)) + (not solid_gold) * (50 + 7 * len(engraving))
+    return cost
+
+
+print(cost_of_project("Charlie+Denver", True))
+
