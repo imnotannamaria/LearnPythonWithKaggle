553baadc5c560bb8ae3ee63924bd4d87250b38f3
Anna Maria <aannamariabr@outlook.com>
2023-06-26 11:24:49 -0300

    Python - Hello

---
+++ 01_python/hello.py
@@ -0,0 +1,27 @@
+spam_amount = 0
+print(spam_amount)
+
+# Ordering Spam, egg, Spam, Spam, bacon and Spam (4 more servings of Spam)
+spam_amount = spam_amount + 4
+
+if spam_amount > 0:
+    print("But I don't want ANY spam!")
+
+viking_song = "Spam " * spam_amount
+print(viking_song)
+
+# Order of operations
+# PEMDAS -
+# Parentheses,
+# Exponents,
+# Multiplication/Division,
+# Addition/Subtraction.
+
+alice_candies = 30
+bob_candies = 30
+carol_candies = 31
+
+to_smash = (alice_candies + bob_candies + carol_candies) % 3
+
+print(to_smash)
+
